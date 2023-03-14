558bff3f7a2560a08f733826728628df6145ae49
Mark Dexter <a.markdexter@gmail.com>
2023-03-14 21:47:08 +0800

    Create insert_data.sh

---
+++ insert_data.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
+
+# Do not change code above this line. Use the PSQL variable above to query your database.
+
+cat games.csv | while IFS="," read YEAR ROUND
+do 
+  echo $YEAR
+done
